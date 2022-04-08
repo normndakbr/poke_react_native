da5cecd368d9447c1012c69badbdabb15fd7dd0e
Normandia Akbar <normnd.akbr2@gmail.com>
2022-04-08 13:11:33 +0700

    update class component

--- App.js
+++ App.js
@@ -10,7 +10,6 @@ const App = () => {
       <Text>Totodile</Text>
       <Text>Chikorita</Text>
       <Photo />
-      <TextInput style={{ borderWidth: 1 }}></TextInput>
       <PokeBox />
     </View>
   );
@@ -21,12 +20,26 @@ const Home = () => {
 }
 
 const Photo = () => {
-  return <Image source={{ uri: 'https://archives.bulbagarden.net/media/upload/thumb/0/0c/008Wartortle.png/250px-008Wartortle.png' }} style={{ height: 200, width: 200 }} />
+  return (
+    <View>
+      <Image
+        source={{ uri: 'https://archives.bulbagarden.net/media/upload/thumb/0/0c/008Wartortle.png/250px-008Wartortle.png' }}
+        style={{ height: 80, width: 80 }} />
+      <Text>Wartortle</Text>
+    </View>
+  );
 }
 
 class PokeBox extends Component {
   render() {
-    return <Text>This PokeBox is a class component</Text>
+    return (
+      <View>
+        <Image
+          source={{ uri: 'https://archives.bulbagarden.net/media/upload/thumb/7/72/082Magneton.png/250px-082Magneton.png' }}
+          style={{ height: 80, width: 80 }} />
+        <Text>Magneton</Text>
+      </View>
+    );
   }
 }
 
