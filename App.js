a53fb035f45e9fecdbf3aa1aa821e67c2c7f1860
Normandia Akbar <normnd.akbr2@gmail.com>
2022-04-08 11:59:16 +0700

    added View with style

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { Text, View } from 'react-native';
 const App = () => {
   return (
     <View>
+      <View style={{ width: 80, height: 80, backgroundColor: '#74b9ff' }}></View>
       <Text>Cyndaquil</Text>
       <Home />
       <Text>Totodile</Text>
