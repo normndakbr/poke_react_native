8a5a7e684adfa6d2048096a7a3f022ca78afe593
Normandia Akbar <normnd.akbr2@gmail.com>
2022-04-08 11:55:08 +0700

    added other component inside App component

--- App.js
+++ App.js
@@ -5,10 +5,15 @@ const App = () => {
   return (
     <View>
       <Text>Cyndaquil</Text>
+      <Home />
       <Text>Totodile</Text>
       <Text>Chikorita</Text>
     </View>
   );
+}
+
+const Home = () => {
+  return <Text>Pikachu</Text>
 }
 
 export default App;
